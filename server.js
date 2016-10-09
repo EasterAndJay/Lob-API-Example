6e28dad5aa0352ffa2e98128f360b22c16634c88
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-10-08 17:43:33 -0700

    Add production deployment configuration

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 var express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 var app = express();
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  stats: {
-      colors: true
-  }
-}).listen(8080, 'localhost', function (err) {
-    if (err) {
-        console.log(err);
+if (process.env.NODE_ENV == JSON.stringify('production')) {
+  app.use(express.static(__dirname));
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname,'index.html'));
+  });
+  app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+  });
+} else {
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+        colors: true
     }
-
-  console.log('Listening at localhost:8080');
-});
-
-
+  }).listen(port, 'localhost', function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at localhost:' + port);
+  });
+};
